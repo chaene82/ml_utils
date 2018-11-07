4070ab09d83fb9a17503a550b14906ec52bad975
Christoph Haene <christoph.haene@gmail.com>
2018-11-07 10:38:27 +0100

    change model path

--- tests/test_ml_utils.py
+++ tests/test_ml_utils.py
@@ -9,9 +9,6 @@ import pytest
 from ml_utils.features import print_features_importances
 from sklearn.externals import joblib
 
-model = joblib.load('C:/Users/haenec/python/ds_early_vip_machinelearning/models/EarlyVIP_Predicitions_RFClassifier.sav')
-
-
 @pytest.fixture
 def test_features_importance():
     model = joblib.load('model/test_model.sav')
