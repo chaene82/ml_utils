b923dfe2423116948e060da3b005ce21791564c0
Christoph Haene <christoph.haene@gmail.com>
2018-11-07 10:59:19 +0100

    add package to test

--- tests/test_ml_utils.py
+++ tests/test_ml_utils.py
@@ -11,7 +11,16 @@ from ml_utils.features import print_features_importances
 from sklearn.externals import joblib
 
 @pytest.fixture
+def response():
+    """Sample pytest fixture.
+
+    See more at: http://doc.pytest.org/en/latest/fixture.html
+    """
+    # import requests
+    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+
 def test_features_importance():
     model = joblib.load('model/test_model.sav')
     labels = ['one','two','tree','four','five', 'six']
     print_features_importances(model, labels)
+    assert 2 >= 1
